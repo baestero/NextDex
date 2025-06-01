7265355a54d8bb83741960856204550fc275ad90
baestero <leonardobaestero@gmail.com>
2025-06-01 13:58:18 -0300

    atualização do layout

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import styles from "./Pokemon.module.css";
 
 const Pokemon = ({ pokemon }) => {
   return (
-    <div>
-      {pokemon && <h1>{pokemon.name}</h1>}
-      {pokemon && (
-        <img
-          src={pokemon.sprites.other["official-artwork"].front_default}
-          alt={pokemon.name}
-        />
-      )}
-    </div>
+    <>
+      <p className={styles.pBusca}>Resultados da busca</p>
+      <div className={styles.pokemonContainer}>
+        <div className={styles.pokemonCard}>
+          {pokemon && (
+            <img
+              src={pokemon.sprites.other["official-artwork"].front_default}
+              alt={pokemon.name}
+            />
+          )}
+          {pokemon && <h3>{pokemon.name}</h3>}
+          {pokemon && <p>{pokemon.types[0].type.name}</p>}
+        </div>
+        <div className={styles.pokemonCard}></div>
+        <div className={styles.pokemonCard}></div>
+        <div className={styles.pokemonCard}></div>
+      </div>
+    </>
   );
 };
 
